554688661254263fa80578db360b34374eaf1b75
mdvh18 <feshitxxxx2@gmail.com>
2026-02-26 11:55:39 +0700

    Create pitumdgame.js

---
+++ pitumdgame.js
@@ -0,0 +1,13 @@
+const PITU_DATABASE = [
+    {
+        name: "JIKAGE ARC 2",
+        banner: "https://link-anh-banner-to.webp",
+        previews: [ 
+            "https://preview-1.webp",
+            "https://preview-2.webp",
+            "https://preview-3.webp",
+            "https://preview-4.webp"
+        ],
+        downloadLink: "https://mdvh18.com/link-tai-game"
+    },
+];
