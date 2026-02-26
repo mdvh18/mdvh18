81db1f4fcc4d0a188320d6be8e039e6c097fdf10
mdvh18 <feshitxxxx2@gmail.com>
2026-02-26 12:35:20 +0700

    Update pitumdgame.js

--- pitumdgame.js
+++ pitumdgame.js
@@ -1,13 +1,13 @@
 const PITU_DATABASE = [
     {
-        id: "jikage-arc-2", 
-        name: "JIKAGE ARC 2",
-        banner: "https://link-anh-banner.webp",
+        id: "exaltation.html", 
+        name: "[ENG]  Exaltation! v1.1 - [764MB]",
+        banner: "https://blogger.googleusercontent.com/img/a/AVvXsEjmFbm0ixnbrP9s49H9y2ZKTvAW_MUmGy_5f48ZA0OEaY6OUE8xPXTbSYVXRDO7Y4eC8PqaY4bNB332Rl4aoyDf7n_7LCCbs0r6zzh8UvD_--ipf_oPw1w3AW9BChX_2o1JNWiqXUCXTbMrrS9lJOaL_xPLk-OVPOfIZ-WGJyYg4QXKhJzwMug1atHyJvg=s16000",
         previews: [
-            "https://p1.webp",
-            "https://p2.webp",
-            "https://p3.webp",
-            "https://p4.webp"
+            "https://blogger.googleusercontent.com/img/a/AVvXsEgSQm6vl7IEgp3OtcqWaCl_WSsUMYbuU9SevHotcn3zPMukO_XEyFu5pUitrwrY-I8VIyQzkEq9AMo-W5mcF29xW4JEmMN-H4um53YDRBiSP0YSN3Yaydu40UTJgFVMLJUITKCqO9W_3mYBgid9bDzXbR4JJMFYtAV08gmrcvLTtytJmFnGCnaDUxX5N0I=s16000",
+            "https://blogger.googleusercontent.com/img/a/AVvXsEiLM_waqHmatpbBQXDiAMeIg795i_Rabpaxlgip4CetfB3DLOmkh98DVOOnJrn0R7Y-6_2xicj7NHwm6Ak96rVP2DvgxCb9ukRALHEJflYGRtHJYBcO4b4TnQyHfCUuESfjkb0Y5M7Q8RCYz3H5ZAJgShIsdN_YYNa_WkjUtYpzhAvw6r3WpCJ39tBoaJQ=s16000",
+            "https://blogger.googleusercontent.com/img/a/AVvXsEjkNymu18iUgQh12c9ior8GvR4EIMxHQFRHc4zqglIy3B9XRI6WfghnL_r0E5P5Pt5aYH9AcAKJ2c425VVaxrb3L4P3Gbi57kHv2MDKwph9EhZDqnnd8GE0JiWPXqEpLjY_cJeozIg8xVOKqefZNtTeCi8heDWU9__O_WXbXZBdce0238ct-pGJuiOXHZk=s16000",
+            "https://blogger.googleusercontent.com/img/a/AVvXsEjZSI7CBjVZtLErdl0b4fSMtuKTeVhguN88oTS2yQTNzya3r2DbfXH27_4_uPRTjH-oO3sJTw7hmDOWI2woh00cVFv293EhyPGEfRcHogdWIlbZGtMBL5n05WxVVk3Fh8qpNACWinUS6RapufO-uMRlthdsgq60fPcjCnCO7kxq9nRiHjlRD1ZKhkVRHbc=s16000"
         ]
     },
     {
