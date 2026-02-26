d6d340192ed7728fea5d4ad97b962b23ca4c92e9
mdvh18 <feshitxxxx2@gmail.com>
2026-02-26 12:04:14 +0700

    Update pitumdgame.js

--- pitumdgame.js
+++ pitumdgame.js
@@ -1,12 +1,22 @@
+/* DATABASE GAME MDVH18 
+   Quy tắc: id = tên file html (không bao gồm .html)
+*/
 const PITU_DATABASE = [
     {
+        id: "jikage-arc-2", // file jikage-arc-2.html
         name: "JIKAGE ARC 2",
         banner: "https://link-anh-banner-to.webp",
-        previews: [ 
+        previews: [
             "https://preview-1.webp",
             "https://preview-2.webp",
             "https://preview-3.webp",
             "https://preview-4.webp"
         ],
     },
+    {
+        id: "naruto-kunoichi", // file naruto-kunoichi.html
+        name: "NARUTO KUNOICHI TRAINING",
+        banner: "https://link-banner-2.webp",
+        previews: ["https://p1.webp", "https://p2.webp"],
+    }
 ];
